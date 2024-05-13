84ec10827bfdcffa88e33b76592d306efe847bf5
justus.voss <justus.voss@check24.de>
2024-05-13 21:50:33 +0200

    Removed Helmet Completely

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,14 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Helmet } from "react-helmet";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Helmet>
-      <meta charSet="utf-8" />
-      <title>Techblog</title>
-      <link rel="icon" type="image/svg" href="logo.svg"></link>
-    </Helmet>
     <App />
   </React.StrictMode>
 );
