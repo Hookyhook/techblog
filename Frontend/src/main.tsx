00c74227289baf2222d735a1044458a34fc615ae
justus.voss <justus.voss@check24.de>
2024-05-13 16:10:39 +0200

    Updated Metadata

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Helmet } from "react-helmet";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Techblog</title>
+      <link rel="icon" type="image/svg" href="logo.svg"></link>
+    </Helmet>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
